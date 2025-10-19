b68560ed41dbc15cfd592f8195869542f47f8271
Nurhaq Halim <nurhaqhalim11@gmail.com>
2025-10-19 12:10:55 +0700

    feature: Phase 5 add home, detail, and favorite feature UI
    Adds the user interface layouts and base fragments for the home screen, movie detail screen, and the favorite movies screen.
    
    - Creates `HomeFragment` with a layout including a ViewPager for discovering movies and a RecyclerView for recommended movies.
    - Implements `MovieDetailFragment` with a detailed layout for displaying movie information, backdrop, cast, and a booking button.
    - Adds `FavoriteActivity` and its layout to display a list of favorite movies.
    - Introduces a versatile `MovieCardView` component with horizontal and vertical orientations.
    - Adds new layouts for movie items (`item_movie_vertical`), cast (`item_cast_movie`), and others.
    - Removes example test files.

---
+++ app/src/main/java/com/halimjr11/cameo/view/feature/detail/MovieDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.halimjr11.cameo.view.feature.detail
+
+import androidx.lifecycle.ViewModel
+import com.halimjr11.cameo.components.BaseFragment
+import com.halimjr11.cameo.databinding.FragmentMovieDetailBinding
+
+class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, ViewModel>(
+    FragmentMovieDetailBinding::inflate
+) {
+
+}
---
+++ app/src/main/java/com/halimjr11/cameo/view/feature/home/DiscoverMovieFragment.kt
@@ -0,0 +1,9 @@
+package com.halimjr11.cameo.view.feature.home
+
+import androidx.lifecycle.ViewModel
+import com.halimjr11.cameo.components.BaseFragment
+import com.halimjr11.cameo.databinding.FragmentDiscoverMovieBinding
+
+class DiscoverMovieFragment : BaseFragment<FragmentDiscoverMovieBinding, ViewModel>(
+    FragmentDiscoverMovieBinding::inflate
+) {}

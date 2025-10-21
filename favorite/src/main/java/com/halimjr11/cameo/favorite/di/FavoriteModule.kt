1f88bb3cdec6ad1827c5c169efae291891af9742
Nurhaq Halim <nurhaqhalim11@gmail.com>
2025-10-21 18:59:01 +0700

    refactor: scope favorite dependencies
    Scope FavoriteViewModel and GetFavoriteUseCase to FavoriteActivity's lifecycle.

--- favorite/src/main/java/com/halimjr11/cameo/favorite/di/FavoriteModule.kt
+++ favorite/src/main/java/com/halimjr11/cameo/favorite/di/FavoriteModule.kt
@@ -1,12 +1,17 @@
 package com.halimjr11.cameo.favorite.di
 
+import com.halimjr11.cameo.domain.usecase.GetFavoriteUseCase
+import com.halimjr11.cameo.favorite.FavoriteActivity
 import com.halimjr11.cameo.favorite.viewmodel.FavoriteViewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
 fun loadFavoriteModule() {
     val favoriteModule = module {
-        single { FavoriteViewModel(get()) }
+        scope<FavoriteActivity> { scoped { FavoriteViewModel(get()) } }
     }
-    loadKoinModules(favoriteModule)
+    val useCase = module {
+        scope<FavoriteActivity> { scoped { GetFavoriteUseCase(get()) } }
+    }
+    loadKoinModules(listOf(useCase, favoriteModule))
 }
